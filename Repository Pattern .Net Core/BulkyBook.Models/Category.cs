ad496abc82a11a7f34f20a96b08c3606abf3d393
qasim <qasimzaildar2@gmail.com>
2023-01-29 15:59:32 -0800

    first commit

---
+++ Repository Pattern .Net Core/BulkyBook.Models/Category.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyBook.Models
+{
+    public class Category
+    {
+
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string? Name { get; set; }
+        public int DisplayName { get; set; }
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+
+
+    }
+}
